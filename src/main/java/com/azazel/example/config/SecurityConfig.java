84019b1ef6e46479e01a5771309976da3da88e99
Alimar555 <101833808+Alimar555@users.noreply.github.com>
2024-12-13 12:59:09 +0500

    Update SecurityConfig.java

--- src/main/java/com/azazel/example/config/SecurityConfig.java
+++ src/main/java/com/azazel/example/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.azazel.example.config;
 
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,21 +13,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-import static com.azazel.example.domain.Permission.ADMIN_CREATE;
-import static com.azazel.example.domain.Permission.ADMIN_DELETE;
-import static com.azazel.example.domain.Permission.ADMIN_READ;
-import static com.azazel.example.domain.Permission.ADMIN_UPDATE;
-import static com.azazel.example.domain.Permission.MANAGER_CREATE;
-import static com.azazel.example.domain.Permission.MANAGER_DELETE;
-import static com.azazel.example.domain.Permission.MANAGER_READ;
-import static com.azazel.example.domain.Permission.MANAGER_UPDATE;
-import static com.azazel.example.domain.Role.ADMIN;
-import static com.azazel.example.domain.Role.MANAGER;
-import static org.springframework.http.HttpMethod.DELETE;
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpMethod.PUT;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import static com.azazel.example.domain.enums.Role.*;
+
 
 @Configuration
 @EnableWebSecurity
@@ -45,24 +34,24 @@ public class SecurityConfig {
             "/swagger-ui/**",
             "/webjars/**",
             "/swagger-ui.html"};
+
+
     private final JwtAuthFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers(WHITE_LIST_URL)
-                                .permitAll()
-                                .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
-                                .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
-                                .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
-                                .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
-                                .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
-                                .anyRequest()
-                                .authenticated()
+                        req.requestMatchers(WHITE_LIST_URL).permitAll()
+                                .requestMatchers("/api/v1/products/**").hasRole(ADMIN.name())
+                                .requestMatchers("/api/v1/workers/**").hasRole(ADMIN.name())
+                                .requestMatchers("/api/v1/transactions/**").hasAnyRole(ADMIN.name(), CASHIER.name())
+                                .requestMatchers("/api/v1/revisions/**").hasAnyRole(ADMIN.name(), ACCOUNTANT.name())
+                                .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
@@ -71,9 +60,10 @@ public class SecurityConfig {
                         logout.logoutUrl("/api/v1/auth/logout")
                                 .addLogoutHandler(logoutHandler)
                                 .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
-                )
-        ;
+                );
 
         return http.build();
     }
 }
+
+
